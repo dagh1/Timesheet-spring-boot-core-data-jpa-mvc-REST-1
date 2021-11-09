ab5791a2b20acfe9aeb5d91b55b628f96483aca1
dagh1 <dalirecardo@gmail.com>
2021-11-09 14:02:10 +0100

    aaa

--- src/main/java/tn/esprit/spring/TimesheetSpringBootCoreDataJpaMvcRest1Application.java
+++ src/main/java/tn/esprit/spring/TimesheetSpringBootCoreDataJpaMvcRest1Application.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 public class TimesheetSpringBootCoreDataJpaMvcRest1Application {
 
 	public static void main(String[] args) {
-		// test w
+		// test soket
 		SpringApplication.run(TimesheetSpringBootCoreDataJpaMvcRest1Application.class, args);
 	}
 
